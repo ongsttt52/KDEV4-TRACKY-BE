db0add5cabf3238b88b47c577ef8d4d2c0681f94
Tojaman <zzoon5020@naver.com>
2025-03-21 17:12:19 +0900

    jenkins ecs 배포 테스트

--- tracky-emulator/src/main/java/kernel360/trackyemulator/TrackyEmulatorApplication.java
+++ tracky-emulator/src/main/java/kernel360/trackyemulator/TrackyEmulatorApplication.java
@@ -2,12 +2,22 @@ package kernel360.trackyemulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TrackyEmulatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrackyEmulatorApplication.class, args);
+    }
+    @RestController
+    public static class HelloController {
+
+        @GetMapping("/")
+        public String hello() {
+            return "안녕";
+        }
     }
 
 }
--- tracky-web/src/main/java/kernel360/trackyweb/TrackyWebApplication.java
+++ tracky-web/src/main/java/kernel360/trackyweb/TrackyWebApplication.java
@@ -2,6 +2,8 @@ package kernel360.trackyweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TrackyWebApplication {
@@ -9,5 +11,12 @@ public class TrackyWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrackyWebApplication.class, args);
 	}
+	@RestController
+	public static class HelloController {
 
+		@GetMapping("/")
+		public String hello() {
+			return "안녕";
+		}
+	}
 }
