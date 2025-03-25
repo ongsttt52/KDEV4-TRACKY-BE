b4985d4f52a5980e4d514ea773eb7d587e0f7e87
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-25 14:28:50 +0900

    feat/ cors local에서는 front proxy로 처리하도록 webconfig 처리

--- tracky-web/src/main/java/kernel360/trackyweb/config/WebConfig.java
+++ tracky-web/src/main/java/kernel360/trackyweb/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:5173", "https://tracky-fe.vercel.app/") // React (Vite) 주소
+			.allowedOrigins("https://tracky-fe.vercel.app") // React (Vite) 주소
 			.allowedMethods("*")
 			.allowedHeaders("*")
 			.allowCredentials(true);
