06311d9576b8de87fe58fa3bda6a62b0441cc53a
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-28 11:03:36 +0900

    feat: 공통 에러처리 예시 성공

--- tracky-core/src/main/java/kernel360/trackycore/core/infrastructure/entity/GpsHistory.java
+++ tracky-core/src/main/java/kernel360/trackycore/core/infrastructure/entity/GpsHistory.java
@@ -13,6 +13,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -31,7 +32,7 @@ public class GpsHistory {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "drive_id")
+	@JoinColumn(name = "drive_id")
 	@ManyToOne(fetch = FetchType.LAZY)
 	private DriveEntity drive;
 
