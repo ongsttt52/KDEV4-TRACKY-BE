865fe829cc6ef6017b28508598f1aeda7be44ed9
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-29 22:18:48 +0900

    feat: #33 carcontroller error 처리

--- tracky-web/src/main/java/kernel360/trackyweb/car/presentation/CarController.java
+++ tracky-web/src/main/java/kernel360/trackyweb/car/presentation/CarController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kernel360.trackycore.core.common.ApiResponse;
 import kernel360.trackyweb.car.application.CarService;
 import kernel360.trackyweb.car.application.dto.CarDetailResponse;
 import kernel360.trackyweb.car.application.dto.CarResponse;
@@ -24,13 +25,13 @@ public class CarController {
 
 	@GetMapping("all")
 	@Operation(summary = "전체 차량 조회", description = "DB에 있는 모든 차량 리스트 조회")
-	public List<CarResponse> getAll() {
+	public ApiResponse<List<CarResponse>> getAll() {
 		return carService.getAll();
 	}
 
 	@GetMapping("searchbymdn/{keyword}")
 	@Operation(summary = "전체 차량 조회", description = "DB에 있는 모든 차량 리스트 조회")
-	public List<CarResponse> searchByMdn(
+	public ApiResponse<List<CarResponse>> searchByMdn(
 		@PathVariable String keyword
  	) {
 		return carService.searchByMdn(keyword);
@@ -38,7 +39,7 @@ public class CarController {
 
 	@GetMapping("searchbyid/{id}")
 	@Operation(summary = "단건 차량 조회", description = "DB에 있는 차량 중 id로 상세 조회")
-	public CarResponse searchById(
+	public ApiResponse<CarResponse> searchById(
 		@PathVariable Long id
 	) {
 		return carService.searchById(id);
@@ -46,7 +47,7 @@ public class CarController {
 
 	@GetMapping("searchbyid/detail/{id}")
 	@Operation(summary = "단건 차량 조회", description = "DB에 있는 차량 중 id로 상세 조회, device 정보 포함")
-	public CarDetailResponse searchDetailById(
+	public ApiResponse<CarDetailResponse> searchDetailById(
 		@PathVariable Long id
 	) {
 		return carService.searchDetailById(id);
