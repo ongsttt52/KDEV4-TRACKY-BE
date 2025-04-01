984a94c2409b3287186fbb9ca0316a98b9600351
devJeenee <eui415566@gmail.com>
2025-04-01 19:18:51 +0900

    feat #64 복합키 설정

--- tracky-core/src/main/java/kernel360/trackycore/core/common/entity/GpsHistoryEntity.java
+++ tracky-core/src/main/java/kernel360/trackycore/core/common/entity/GpsHistoryEntity.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -24,6 +25,7 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@IdClass(GpsHistoryId.class)
 public class GpsHistoryEntity {
 
 	@Id
