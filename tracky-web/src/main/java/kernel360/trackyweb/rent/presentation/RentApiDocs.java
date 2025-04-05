3a1693dcb2866796f38576b1ff4e4cf906278aa9
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-04-05 23:13:12 +0900

    feat: #105 예약 상태 & 날짜 기준 검색 추가

--- tracky-web/src/main/java/kernel360/trackyweb/rent/presentation/RentApiDocs.java
+++ tracky-web/src/main/java/kernel360/trackyweb/rent/presentation/RentApiDocs.java
@@ -1,9 +1,12 @@
 package kernel360.trackyweb.rent.presentation;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,9 +20,19 @@ public interface RentApiDocs {
 	@Operation(summary = "대여 목록 전체 조회", description = "DB에 있는 모든 대여 리스트 조회")
 	ApiResponse<List<RentResponse>> getAll();
 
+	@Operation(summary = "rent uuid, status, date 필터링 검색", description = "rent uuid, status, date 필터링 검색")
+	ApiResponse<List<RentResponse>> searchByFilter(
+		@RequestParam String rentUuid,
+		@RequestParam(required = false, name = "status") String rentStatus,
+		@RequestParam(required = false, name = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate rentDate // yyyy-MM-dd
+	);
+
 	@Operation(summary = "대여 신규 등록", description = "대여 신규 등록 API")
 	ApiResponse<RentResponse> create(@RequestBody RentRequest rentRequest);
 
 	@Operation(summary = "대여 UUID로 단건 조회", description = "rent_uuid로 단일 대여 조회 API")
-	ApiResponse<List<RentResponse>> searchByRentUuid(@PathVariable String rentUuid);
+	ApiResponse<RentResponse> searchDetailByRentUuid(@PathVariable String rentUuid);
+
+	@Operation(summary = "대여 삭제", description = "대여 정보 삭제 API")
+	ApiResponse<String> delete(@PathVariable String rentUuid);
 }
