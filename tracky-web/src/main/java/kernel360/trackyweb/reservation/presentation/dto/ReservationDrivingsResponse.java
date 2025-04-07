c2ff33090cf2a6d13dfbcb4ce4295ea6b2984779
devJeenee <eui415566@gmail.com>
2025-04-07 21:50:18 +0900

    feat #88 차량 이용자 전용 화면 개발

--- tracky-web/src/main/java/kernel360/trackyweb/reservation/presentation/dto/ReservationDrivingsResponse.java
+++ tracky-web/src/main/java/kernel360/trackyweb/reservation/presentation/dto/ReservationDrivingsResponse.java
@@ -1,17 +1,18 @@
 package kernel360.trackyweb.reservation.presentation.dto;
 
 import java.time.LocalDateTime;
+
 import kernel360.trackycore.core.common.entity.DriveEntity;
 import kernel360.trackycore.core.common.entity.LocationEntity;
 
 public record ReservationDrivingsResponse(
 	LocalDateTime driveOnTime,    //주행 시작시간
 	LocalDateTime driveOffTim,    //주행 종료 시간
-	double driveDistance,    //주행 별 거리
+	Double driveDistance,    //주행 별 거리
 	int driveStartLon,    //시작 위도
 	int driveStartLat,    //시작 경도
-	int driveEndLon,    //종료 위도
-	int driveEndLat //종료 경도
+	Integer driveEndLon,    //종료 위도
+	Integer driveEndLat //종료 경도
 
 ) {
 	public static ReservationDrivingsResponse fromEntity(DriveEntity driveEntity, LocationEntity locationEntity) {
