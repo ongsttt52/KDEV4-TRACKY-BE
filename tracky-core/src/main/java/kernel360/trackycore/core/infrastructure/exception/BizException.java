a6bcb06cc3f6ada5af12fe5c58d4b48844ed2afd
devJeenee <eui415566@gmail.com>
2025-04-07 18:15:23 +0900

    core-exception 수정

--- tracky-core/src/main/java/kernel360/trackycore/core/infrastructure/exception/BizException.java
+++ tracky-core/src/main/java/kernel360/trackycore/core/infrastructure/exception/BizException.java
@@ -1,7 +1,11 @@
 package kernel360.trackycore.core.infrastructure.exception;
 
-public class BizException extends RuntimeException {
-  public BizException(String message) {
-    super(message);
-  }
+public class BizException extends GlobalException {
+	public BizException(ErrorCode errorCode) {
+		super(errorCode);
+	}
+
+	public static BizException notFound() {
+		return new BizException(ErrorCode.BIZ_NOT_FOUND);
+	}
 }
