60e345ca0fad71a22d8e5e8ffefe12f6fbc451c3
Tojaman <zzoon5020@naver.com>
2025-04-09 20:52:17 +0900

    fix: Async 설정 수정 및 GpsHistory 복합키 제거

--- tracky-consumer/src/main/java/kernel360/trackyconsumer/config/AsyncConfig.java
+++ tracky-consumer/src/main/java/kernel360/trackyconsumer/config/AsyncConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -15,21 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AsyncConfig {
 
-    @Bean(name = "taskExecutor")ㅇ
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors() * 10);
-        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() * 20);
-        executor.setQueueCapacity(20000);
-        executor.setThreadNamePrefix("AsyncThread-");
-        
-        // 설정 정보 로깅
-        log.info("Available processors: {}", Runtime.getRuntime().availableProcessors());
-        log.info("Core pool size: {}", executor.getCorePoolSize());
-        log.info("Max pool size: {}", executor.getMaxPoolSize());
-        log.info("Queue capacity: {}", executor.getQueueCapacity());
-        
-        executor.initialize();
-        return executor;
-    }
+	@Bean(name = "taskExecutor")
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(Runtime.getRuntime().availableProcessors() * 10);
+		executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() * 20);
+		executor.setQueueCapacity(20000);
+		executor.setThreadNamePrefix("AsyncThread-");
+
+		// 설정 정보 로깅
+		log.info("Available processors: {}", Runtime.getRuntime().availableProcessors());
+		log.info("Core pool size: {}", executor.getCorePoolSize());
+		log.info("Max pool size: {}", executor.getMaxPoolSize());
+		log.info("Queue capacity: {}", executor.getQueueCapacity());
+
+		executor.initialize();
+		return executor;
+	}
 }
