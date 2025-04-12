e0de2895e6e1cb7bfc858b5ea91d1ba25d4b48cb
tojaman <zzoon5020@naver.com>
2025-04-12 14:37:01 +0900

    refactor #148 hub, consumer 모듈 RabbitMQConfig 수정
    각각 필요한 Bean만 주입하도록 수정

--- tracky-consumer/src/main/java/kernel360/trackyconsumer/config/RabbitMQConfig.java
+++ tracky-consumer/src/main/java/kernel360/trackyconsumer/config/RabbitMQConfig.java
@@ -4,8 +4,6 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,7 +37,7 @@ public class RabbitMQConfig {
 	}
 	
 	@Bean
-	public Binding onOffBinding(Queue onOffQueue, TopicExchange exchange) {
+	public Binding onBinding(Queue onOffQueue, TopicExchange exchange) {
 		return BindingBuilder.bind(onOffQueue).to(exchange).with("on");
 	}
 	
@@ -48,26 +46,8 @@ public class RabbitMQConfig {
 		return BindingBuilder.bind(onOffQueue).to(exchange).with("off");
 	}
 
-	// @Bean
-	// public Queue queue() {
-	// 	return new Queue(QUEUE_NAME, true);
-	// }
-
-	// @Bean
-	// public Binding binding(Queue queue, TopicExchange exchange) {
-	// 	return BindingBuilder.bind(queue).to(exchange).with("#");
-	// }
-
 	@Bean
 	public Jackson2JsonMessageConverter messageConverter() {
 		return new Jackson2JsonMessageConverter();
-	}
-
-	// ConnectionFactory는 Spring Boot에서 자동으로 생성해주므로 주입받아 사용한다.
-	@Bean
-	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-		rabbitTemplate.setMessageConverter(messageConverter());
-		return rabbitTemplate;
 	}
 }
--- tracky-hub/src/main/java/kernel360trackybe/trackyhub/config/RabbitMQConfig.java
+++ tracky-hub/src/main/java/kernel360trackybe/trackyhub/config/RabbitMQConfig.java
@@ -1,8 +1,5 @@
 package kernel360trackybe.trackyhub.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -14,55 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
 	public static final String EXCHANGE_NAME = "car-info-exchange";
-	// public static final String QUEUE_NAME = "car-info-queue";
 	public static final String GPS_QUEUE_NAME = "gps-queue";
 	public static final String ONOFF_QUEUE_NAME = "on-off-Queue";
-	// public static final String OFF_QUEUE_NAME = "off-queue";
 
 	@Bean
 	public TopicExchange exchange() {
 		return new TopicExchange(EXCHANGE_NAME);
 	}
-
-	@Bean
-	public Queue gpsQueue() {
-		return new Queue(GPS_QUEUE_NAME, true);
-	}
-
-	@Bean
-	public Queue onOffQueue() {
-		return new Queue(ONOFF_QUEUE_NAME, true);
-	}
-
-	// @Bean
-	// public Queue offQueue() {
-	// 	return new Queue(OFF_QUEUE_NAME, true);
-	// }
-
-	@Bean
-	public Binding gpsBinding(Queue gpsQueue, TopicExchange exchange) {
-		return BindingBuilder.bind(gpsQueue).to(exchange).with("gps");
-	}
-
-	@Bean
-	public Binding onOffBinding(Queue onOffQueue, TopicExchange exchange) {
-		return BindingBuilder.bind(onOffQueue).to(exchange).with("on");
-	}
-
-	// @Bean
-	// public Binding offBinding(Queue offQueue, TopicExchange exchange) {
-	// 	return BindingBuilder.bind(offQueue).to(exchange).with("off");
-	// }
-
-	// @Bean
-	// public Queue queue() {
-	// 	return new Queue(QUEUE_NAME, true);
-	// }
-
-	// @Bean
-	// public Binding binding(Queue queue, TopicExchange exchange) {
-	// 	return BindingBuilder.bind(queue).to(exchange).with("#");
-	// }
 
 	@Bean
 	public Jackson2JsonMessageConverter messageConverter() {
