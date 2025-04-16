3a1c178739bb93bdc4b74fbe65c5220eb269943c
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-04-16 15:47:45 +0900

    refactor: refactoring car

--- tracky-core/src/main/java/kernel360/trackycore/core/common/provider/CarProvider.java
+++ tracky-core/src/main/java/kernel360/trackycore/core/common/provider/CarProvider.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 import kernel360.trackycore.core.common.entity.CarEntity;
+import kernel360.trackycore.core.infrastructure.exception.CarException;
+import kernel360.trackycore.core.infrastructure.exception.ErrorCode;
 import kernel360.trackycore.core.infrastructure.repository.CarRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +17,16 @@ public class CarProvider {
 	private final CarRepository carRepository;
 
 	// save and update
-	public CarEntity saveCar(CarEntity car) {
+	public CarEntity save(CarEntity car) {
 		return carRepository.save(car);
 	}
 
-	public Optional<CarEntity> findByMdn(String mdn) {
+	public Optional<CarEntity> findByMdnOps(String mdn) {
 		return carRepository.findByMdn(mdn);
+	}
+
+	public CarEntity findByMdn(String mdn) {
+		return carRepository.findByMdn(mdn).orElseThrow(() -> CarException.sendError(ErrorCode.CAR_NOT_FOUND));
 	}
 
 	public boolean existsByMdn(String mdn) {
