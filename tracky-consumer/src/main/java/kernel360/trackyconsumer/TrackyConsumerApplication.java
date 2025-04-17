0345d64bfc1c5b654aa1b03ce5d62735d38f8821
Tojaman <zzoon5020@naver.com>
2025-04-17 18:53:58 +0900

    refactor #161 불필요한 코드 제거

--- tracky-consumer/src/main/java/kernel360/trackyconsumer/TrackyConsumerApplication.java
+++ tracky-consumer/src/main/java/kernel360/trackyconsumer/TrackyConsumerApplication.java
@@ -10,8 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {
 	"kernel360.trackycore.core.common",
 })
-// @EnableJpaRepositories를 이용하여 명시적으로 스캔할 패키지를 지정하면 지정된 패키지를 제외한 패키지는 무시한다.
-// 따라서 자기 자신의 패키지로 명시적으로 지정해야 한다.
 @EnableJpaRepositories(basePackages = {
 	"kernel360.trackycore.core.infrastructure",
 	"kernel360.trackyconsumer.infrastructure"
