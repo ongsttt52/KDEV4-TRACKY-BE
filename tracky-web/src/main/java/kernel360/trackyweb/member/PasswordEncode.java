49dd7f137f8e10d0e0270b26c82df6410de7548c
ji-won-2 <koojiwon990321@gmail.com>
2025-04-17 21:30:38 +0900

    refactor: passwordencode

--- tracky-web/src/main/java/kernel360/trackyweb/member/PasswordEncode.java
+++ tracky-web/src/main/java/kernel360/trackyweb/member/PasswordEncode.java
@@ -3,10 +3,11 @@ package kernel360.trackyweb.member;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class PasswordTest {
+public class PasswordEncode {
 	public static void main(String[] args) {
 		String encoded = new BCryptPasswordEncoder().encode("121212");
 		System.out.println(encoded);
 	}
 }
+
 */
