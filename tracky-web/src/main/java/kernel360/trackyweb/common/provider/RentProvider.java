77fb8f8f06888d0725cf44710abc46c3ba558cfa
ji-won-2 <koojiwon990321@gmail.com>
2025-04-17 21:03:32 +0900

    refactor: RentProvider 수정

--- tracky-web/src/main/java/kernel360/trackyweb/common/provider/RentProvider.java
+++ tracky-web/src/main/java/kernel360/trackyweb/common/provider/RentProvider.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import kernel360.trackycore.core.common.exception.ErrorCode;
 import kernel360.trackycore.core.common.exception.GlobalException;
-import kernel360.trackycore.core.infrastructure.repository.CarRepository;
 import kernel360.trackyweb.common.entity.RentEntity;
 import kernel360.trackyweb.common.repository.RentRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +13,9 @@ import lombok.RequiredArgsConstructor;
 public class RentProvider {
 
 	private final RentRepository rentRepository;
-	private final CarRepository carRepository;
 
 	public RentEntity findByRentUuid(String rentUuid) {
 		return rentRepository.findByRentUuid(rentUuid)
 			.orElseThrow(() -> GlobalException.throwError(ErrorCode.RENT_NOT_FOUND));
-	}
-
-	public RentEntity save(RentEntity rent) {
-		return rentRepository.save(rent);
 	}
 }
