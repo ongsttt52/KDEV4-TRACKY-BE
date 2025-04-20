e1239c9ed04a1fbc400244ba51cb1b57d470ef64
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-04-20 18:47:08 +0900

    feat: 운행 기록 detail 기존 로직 -> 추후 dsl 로 변동해야함

--- tracky-web/src/main/java/kernel360/trackyweb/drive/presentation/DriveController.java
+++ tracky-web/src/main/java/kernel360/trackyweb/drive/presentation/DriveController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import kernel360.trackyweb.drive.application.DriveService;
 import kernel360.trackyweb.drive.application.dto.request.DriveSearchFilterRequest;
 import kernel360.trackyweb.drive.application.dto.response.CarListResponse;
 import kernel360.trackyweb.drive.application.dto.response.DriveListResponse;
+import kernel360.trackyweb.drive.domain.DriveHistory;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -34,5 +36,13 @@ public class DriveController {
 	) {
 		DriveSearchFilterRequest driveSearchFilterRequest = new DriveSearchFilterRequest(search, pageable);
 		return driveService.getBySearchFilter(driveSearchFilterRequest);
+	}
+
+	@GetMapping("/{driveId}")
+	public ApiResponse<DriveHistory> getDriveHistroy(
+		@PathVariable Long driveId
+	) {
+		DriveHistory history = driveService.getDriveHistroy(driveId);
+		return ApiResponse.success(history);
 	}
 }
