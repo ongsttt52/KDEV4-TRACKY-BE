c8c9dd5e9ffc89aea1ad09bf0cc3a0a845810721
kimchijinju <hanbimkim@gmail.com>
2025-04-22 18:37:35 +0900

    feat: Web 및 Consumer 모듈 핸들링 하지 않은 에러 알림 전송

--- tracky-consumer/src/main/java/kernel360/trackyconsumer/common/exception/ConsumerGlobalExceptionHandler.java
+++ tracky-consumer/src/main/java/kernel360/trackyconsumer/common/exception/ConsumerGlobalExceptionHandler.java
@@ -4,12 +4,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import kernel360.trackycore.core.common.webhook.Notifier;
+import lombok.RequiredArgsConstructor;
+
 @RestControllerAdvice
 @io.swagger.v3.oas.annotations.Hidden
+@RequiredArgsConstructor
 public class ConsumerGlobalExceptionHandler {
+	private final Notifier notifier;
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception e) {
+		notifier.notify(e);
 		return ResponseEntity
 			.internalServerError()
 			.body(ErrorResponse.from(e));
