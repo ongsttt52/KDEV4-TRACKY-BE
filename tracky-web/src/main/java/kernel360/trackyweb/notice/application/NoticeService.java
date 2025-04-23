7e6de08e9935c37f2b19eec614eb6b6bc271c3c2
Tojaman <zzoon5020@naver.com>
2025-04-23 19:29:35 +0900

    feat #228 Notice CRUD 구현

---
+++ tracky-web/src/main/java/kernel360/trackyweb/notice/application/NoticeService.java
@@ -0,0 +1,76 @@
+package kernel360.trackyweb.notice.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import kernel360.trackycore.core.common.api.ApiResponse;
+import kernel360.trackycore.core.domain.entity.MemberEntity;
+import kernel360.trackycore.core.domain.entity.NoticeEntity;
+import kernel360.trackyweb.notice.application.dto.request.NoticeRequest;
+import kernel360.trackyweb.notice.application.dto.response.NoticeDetailResponse;
+import kernel360.trackyweb.notice.domain.provider.NoticeProvider;
+import kernel360.trackyweb.sign.domain.provider.MemberProvider;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class NoticeService {
+	private final NoticeProvider noticeProvider;
+	private final MemberProvider memberProvider;
+
+	@Transactional
+	public ApiResponse<NoticeDetailResponse> register(String memberId, NoticeRequest noticeRequest) {
+
+		MemberEntity member = memberProvider.getMember(memberId);
+
+		NoticeEntity notice = NoticeEntity.create(
+			member,
+			noticeRequest.title(),
+			noticeRequest.content(),
+			noticeRequest.isImportant()
+		);
+
+		NoticeEntity savedNotice = noticeProvider.save(notice);
+
+		NoticeDetailResponse response = NoticeDetailResponse.from(savedNotice);
+		return ApiResponse.success(response);
+	}
+
+	@Transactional
+	public ApiResponse<NoticeDetailResponse> update(Long id, NoticeRequest noticeRequest) {
+
+		NoticeEntity notice = noticeProvider.getById(id);
+		notice.update(noticeRequest.title(), noticeRequest.content(), noticeRequest.isImportant());
+
+		NoticeDetailResponse response = NoticeDetailResponse.from(notice);
+
+		return ApiResponse.success(response);
+	}
+
+	@Transactional
+	public ApiResponse<List<NoticeDetailResponse>> getAll() {
+
+		List<NoticeEntity> notices = noticeProvider.getAll();
+
+		List<NoticeDetailResponse> noticeDetailResponseList = notices.stream()
+			.map(NoticeDetailResponse::from)
+			.toList();
+
+		return ApiResponse.success(noticeDetailResponseList);
+	}
+
+	@Transactional
+	public ApiResponse<NoticeDetailResponse> delete(Long id) {
+
+		NoticeEntity notice = noticeProvider.getById(id);
+
+		notice.softDelete();
+
+		NoticeEntity updatedNotice = noticeProvider.save(notice);
+
+		NoticeDetailResponse response = NoticeDetailResponse.from(updatedNotice);
+		return ApiResponse.success(response);
+	}
+}
