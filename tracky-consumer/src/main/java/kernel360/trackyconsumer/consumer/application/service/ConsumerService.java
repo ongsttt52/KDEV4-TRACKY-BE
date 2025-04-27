7df6556d40ce692fc55ff6573a66f0a32fea112e
tojaman <zzoon5020@naver.com>
2025-04-27 22:57:56 +0900

    feat #244 TimeDistance 시간 계산 로직 추가

--- tracky-consumer/src/main/java/kernel360/trackyconsumer/consumer/application/service/ConsumerService.java
+++ tracky-consumer/src/main/java/kernel360/trackyconsumer/consumer/application/service/ConsumerService.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import kernel360.trackyconsumer.consumer.application.dto.GpsProcessResult;
 import kernel360.trackycore.core.domain.entity.*;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -42,10 +41,10 @@ public class ConsumerService {
 		LocalDateTime oTime = request.oTime();
 		DriveEntity drive = driveProvider.getDrive(car, oTime);
 
-		GpsProcessResult processResult = processGpsRequests(cycleGpsRequestList, drive, oTime);
+		processTimeDistance(cycleGpsRequestList, oTime, car);
 
-		saveTimeDistance(oTime.toLocalDate(), oTime.getHour(), car, processResult.totalDistance());
-		gpsHistoryProvider.saveAll(processResult.gpsHistoryList());
+		List<GpsHistoryEntity> gpsHistories = toGpsHistoryList(cycleGpsRequestList, drive, oTime);
+		gpsHistoryProvider.saveAll(gpsHistories);
 	}
 
 	@Transactional
@@ -79,26 +78,59 @@ public class ConsumerService {
 		location.updateEndLocation(carOnOffRequest.gpsInfo().getLat(), carOnOffRequest.gpsInfo().getLon());
 	}
 
-	private GpsProcessResult processGpsRequests(List<CycleGpsRequest> cycleGpsRequestList, DriveEntity drive, LocalDateTime oTime) {
-		double totalDistance = 0.0;
-		List<GpsHistoryEntity> gpsHistoryList = new ArrayList<>();
+	private List<GpsHistoryEntity> toGpsHistoryList(List<CycleGpsRequest> cycleGpsRequestList,
+													DriveEntity drive,
+													LocalDateTime oTime) {
+		List<GpsHistoryEntity> gpsHistories = new ArrayList<>();
 
-		for (CycleGpsRequest cycleRequest : cycleGpsRequestList) {
-			totalDistance += cycleRequest.gpsInfo().getSum();
-			gpsHistoryList.add(cycleRequest.toGpsHistoryEntity(drive, oTime, cycleRequest.gpsInfo().getSum()));
+		for (int i = 0; i < cycleGpsRequestList.size(); i++) {
+			CycleGpsRequest cycleGpsRequest = cycleGpsRequestList.get(i);
+			gpsHistories.add(cycleGpsRequest.toGpsHistoryEntity(drive, oTime.plusSeconds(i), cycleGpsRequest.gpsInfo().getSum()));
 		}
 
-		return GpsProcessResult.of(gpsHistoryList, totalDistance);
+		return gpsHistories;
+	}
+
+	/**
+	 * 주기 정보(60개)를 순회하며 TimeDistance를 저장한다.
+	 * 만약 시간이 바뀌었다면 바뀐 시간으로 TimeDistance를 저장한다.(if (nowHour != newHour))
+	 * DB 접근을 최소화하기 위해 움직이지 않은 경우엔 저장하지 않는다.(if (distance > 0.0))
+	 */
+	private void processTimeDistance(List<CycleGpsRequest> cycleGpsRequests, LocalDateTime startTime, CarEntity car) {
+
+		double distance = 0.0;
+
+		LocalDate currentDate = startTime.toLocalDate();
+		int currentHour = startTime.getHour();
+
+		int sec = 0;
+		for (CycleGpsRequest cycleGpsRequest : cycleGpsRequests) {
+
+			LocalDateTime nextTime = startTime.plusSeconds(sec++);
+			LocalDate nextDate = nextTime.toLocalDate();
+			int nextHour = nextTime.getHour();
+
+			if (currentHour != nextHour) {
+				if (distance > 0.0) saveTimeDistance(currentDate, currentHour, car, distance);
+
+				distance = 0.0;
+				currentDate = nextDate;
+				currentHour = nextHour;
+			}
+			distance += cycleGpsRequest.gpsInfo().getSum();
+		}
+
+		if (distance > 0.0) saveTimeDistance(currentDate, currentHour, car, distance);
 	}
 
 	private void saveTimeDistance(LocalDate date, int hour, CarEntity car, double totalDistance) {
 
 		timeDistanceProvider.getTimeDistance(date, hour, car)
-			.ifPresentOrElse(
-				timeDistance -> timeDistance.updateDistance(totalDistance),
-				() -> timeDistanceProvider.save(
-					TimeDistanceEntity.create(car, car.getBiz(), date, hour, totalDistance)
-				)
-			);
+				.ifPresentOrElse(
+						timeDistance -> timeDistance.updateDistance(totalDistance),
+						() -> timeDistanceProvider.save(
+								TimeDistanceEntity.create(car, car.getBiz(), date, hour, totalDistance)
+						)
+				);
 	}
 }
