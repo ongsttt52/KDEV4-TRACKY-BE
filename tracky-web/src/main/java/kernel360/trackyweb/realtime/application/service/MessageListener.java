f4f02d337f8716237dc9ee24748934993cb0d669
Tojaman <zzoon5020@naver.com>
2025-04-30 02:46:15 +0900

    feat #275 RabbitMQ 데이터 받아서 Gps 데이터 SSE로 전송

--- tracky-web/src/main/java/kernel360/trackyweb/realtime/application/service/MessageListener.java
+++ tracky-web/src/main/java/kernel360/trackyweb/realtime/application/service/MessageListener.java
@@ -1,10 +1,10 @@
 package kernel360.trackyweb.realtime.application.service;
 
-import java.util.List;
-
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import kernel360.trackyweb.common.sse.GlobalSseEvent;
+import kernel360.trackyweb.emitter.EventEmitterService;
 import kernel360.trackyweb.realtime.application.dto.request.GpsHistoryMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,14 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class MessageListener {
+	private final GlobalSseEvent globalSseEvent;
+	private final EventEmitterService eventEmitterService;
 
 	// GPS 정보 처리 큐
-	@RabbitListener(queues = "web-queue", containerFactory = "batchRabbitListenerContainerFactory")
-	public void receiveCarMessages(List<GpsHistoryMessage> messages) {
-		log.info("GPS 메시지 배치 수신: {} 개", messages.size());
+	@RabbitListener(queues = "web-queue")
+	public void receiveCarMessages(GpsHistoryMessage messages) {
 
-		messages.forEach(message -> {
-			log.info("GPS 메시지 수신: {}", message.toString());
-		});
+		eventEmitterService.sendEvent(messages.mdn(), messages);
+		log.info("Web-Queue 메시지 SSE 전송: {}", messages.toString());
 	}
 }
