0b5f66d13869ffe22050bf6a5210a27017a927f4
devJeenee <eui415566@gmail.com>
2025-05-02 16:37:34 +0900

    Feat: core-statistic provider 추가

---
+++ tracky-core/src/main/java/kernel360/trackycore/core/domain/provider/TimeDistanceProvider.java
@@ -0,0 +1,10 @@
+package kernel360.trackycore.core.domain.provider;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class TimeDistanceProvider {
+}
