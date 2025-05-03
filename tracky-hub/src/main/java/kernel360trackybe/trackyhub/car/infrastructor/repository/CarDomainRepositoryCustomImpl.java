ba07e9d935485b88ae5caf2f9d0c5e645b7f1dfd
tojaman <zzoon5020@naver.com>
2025-05-03 23:55:41 +0900

    refactor #306 Hub 모듈 Car 도메인 디렉토리 분리 및 이름 변경

---
+++ tracky-hub/src/main/java/kernel360trackybe/trackyhub/car/infrastructor/repository/CarDomainRepositoryCustomImpl.java
@@ -0,0 +1,37 @@
+package kernel360trackybe.trackyhub.car.infrastructor.repository;
+
+import static kernel360.trackycore.core.domain.entity.QCarEntity.*;
+import static kernel360.trackycore.core.domain.entity.QRentEntity.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import kernel360.trackycore.core.domain.entity.CarEntity;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class CarDomainRepositoryCustomImpl implements CarDomainRepositoryCustom {
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public List<CarEntity> availableRent() {
+		LocalDateTime now = LocalDateTime.now();
+		return queryFactory
+			.select(carEntity)
+			.from(carEntity)
+			.leftJoin(rentEntity).on(rentEntity.car.eq(carEntity)
+				.and(rentEntity.rentStime.lt(now))
+				.and(rentEntity.rentEtime.gt(now)))
+			.fetch();
+	}
+}
+
+
---
+++ tracky-hub/src/main/java/kernel360trackybe/trackyhub/common/config/RabbitMQProperties.java
@@ -0,0 +1,45 @@
+package kernel360trackybe.trackyhub.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "rabbitmq")
+@Getter
+@Setter
+public class RabbitMQProperties {
+
+	private Exchange exchange;
+	private Queue queue;
+	private Routing routing;
+
+	@Getter
+	@Setter
+	public static class Exchange {
+		private String carInfo;
+		private String cycleInfo;
+		private String dlx;
+	}
+
+	@Getter
+	@Setter
+	public static class Queue {
+		private String gps;
+		private String onoff;
+		private String web;
+		private String dlq;
+	}
+
+	@Getter
+	@Setter
+	public static class Routing {
+		private String gpsKey;
+		private String onKey;
+		private String offKey;
+		private String webKey;
+		private String deadLetterKey;
+	}
+}
