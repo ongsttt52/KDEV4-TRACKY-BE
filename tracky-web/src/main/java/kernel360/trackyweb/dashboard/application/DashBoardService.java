c8cbffd8589354d8afe1f789609fe48362e098c6
택택 <ongsttt52@gmail.com>
2025-05-07 16:08:24 +0900

    refactor 대여 상태 업데이트 더티체킹 방식으로 변경

--- tracky-web/src/main/java/kernel360/trackyweb/dashboard/application/DashBoardService.java
+++ tracky-web/src/main/java/kernel360/trackyweb/dashboard/application/DashBoardService.java
@@ -16,6 +16,7 @@ import kernel360.trackycore.core.domain.entity.RentEntity;
 import kernel360.trackycore.core.domain.entity.enums.CarStatus;
 import kernel360.trackycore.core.domain.entity.enums.RentStatus;
 import kernel360.trackycore.core.domain.provider.CarProvider;
+import kernel360.trackycore.core.domain.provider.RentProvider;
 import kernel360.trackyweb.car.domain.provider.CarDomainProvider;
 import kernel360.trackyweb.dashboard.application.dto.response.ReturnResponse;
 import kernel360.trackyweb.dashboard.domain.CarStatusTemp;
@@ -34,6 +35,7 @@ public class DashBoardService {
 	private final DashGpsHistoryProvider dashGpsHistoryProvider;
 	private final CarProvider carProvider;
 	private final CarDomainProvider carDomainProvider;
+	private final RentProvider rentProvider;
 	private final RentDomainProvider rentDomainProvider;
 	private final DriveDomainProvider driveDomainProvider;
 
@@ -124,9 +126,13 @@ public class DashBoardService {
 		return provinceCountMap;
 	}
 
+	@Transactional
 	public ApiResponse<String> updateStatusToReturn(String rentUuid) {
-		rentDomainProvider.updateRentStatus(rentUuid, RentStatus.RETURNED);
 
+		RentEntity rent = rentProvider.getRent(rentUuid);
+
+		rent.updateStatus(RentStatus.RETURNED);
+		
 		return ApiResponse.success("반납 처리 완료");
 	}
 }
