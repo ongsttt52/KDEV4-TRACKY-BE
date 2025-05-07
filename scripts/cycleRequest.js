06638000302695d7f9942261ee6731b745de0cf8
tojaman <zzoon5020@naver.com>
2025-05-07 09:52:40 +0900

    feat k6-grafana-prometheus 부하테스트 도커 컴포즈 파일일

---
+++ scripts/cycleRequest.js
@@ -0,0 +1,16 @@
+export const requestBody = {
+    mdn: "00000000000",
+    tid: "TID123456",
+    mid: "MID789",
+    pv: "1.0",
+    did: "DEVICE001",
+    oTime: "202504172312",
+    cCnt: 5,
+    cList: [
+        { oTime: "20250417231255", gcd: "A", lat: 37566500, lon: 126978000, ang: "90", spd: "80", sum: 100.0 },
+        { oTime: "20250417231256", gcd: "A", lat: 37566500, lon: 126978000, ang: "90", spd: "90", sum: 100.0 },
+        { oTime: "20250417231257", gcd: "A", lat: 37566500, lon: 126978000, ang: "90", spd: "60", sum: 50.0 },
+        { oTime: "20250417231258", gcd: "A", lat: 37566500, lon: 126978000, ang: "90", spd: "60", sum: 50.0 },
+        { oTime: "20250417231259", gcd: "A", lat: 37566500, lon: 126978000, ang: "90", spd: "60", sum: 50.0 }
+    ]
+};  
---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { requestBody } from './cycleRequest.js';
+
+export const options = {
+    // constant-vus: 지정한 VU(가상 사용자)를 동시에 유지
+    scenarios: {
+        load_test: {
+        executor: 'constant-vus',
+        vus: 1000,           // 동시 1000명
+        duration: '30s',     // 30초 동안 실행
+        },
+    },
+    thresholds: {
+        // 실패율이 1% 초과 시 에러로 간주
+        'http_req_failed': ['rate<0.01'],
+        // 95% 요청 응답 시간이 500ms 이하
+        'http_req_duration': ['p(95)<500'],
+    }, 
+};
+
+export default function () {
+    const url = 'http://host.docker.internal:8082/hub/car/cycle';
+    const payload = JSON.stringify(requestBody);  // 직렬화
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    // 요청을 보낼 URL
+    const res = http.post(url, payload, { headers });
+    // HTTP 200 여부 체크
+    check(res, { 'status is 200': (r) => r.status === 200 });
+}
