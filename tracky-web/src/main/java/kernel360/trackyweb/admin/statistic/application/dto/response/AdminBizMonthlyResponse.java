55b4ac27e21aa41e68a52e13ce46441f34cf0015
택택 <ongsttt52@gmail.com>
2025-05-12 18:13:32 +0900

    fix #315 불필요 주석 제거

--- tracky-web/src/main/java/kernel360/trackyweb/admin/statistic/application/dto/response/AdminBizMonthlyResponse.java
+++ tracky-web/src/main/java/kernel360/trackyweb/admin/statistic/application/dto/response/AdminBizMonthlyResponse.java
@@ -5,26 +5,5 @@ public record AdminBizMonthlyResponse(
 	int month,
 	int driveCount
 ) {
-	// public static List<AdminBizMonthlyResponse> toDto(List<MonthlyStatisticEntity> monthlyStatisticEntities) {
-	// 	if (monthlyStatisticEntities == null || monthlyStatisticEntities.isEmpty()) {
-	// 		return Collections.emptyList();
-	// 	}
-	// 	YearMonth thisMonth = YearMonth.now();
-	// 	YearMonth twelveMonthsAgo = thisMonth.minusMonths(11);
-	//
-	// 	Map<YearMonth, Integer> monthToDriveCount = new HashMap<>();
-	// 	monthlyStatisticEntities.forEach(entity -> {
-	// 		YearMonth yearMonth = YearMonth.from(entity.getDate());
-	// 		monthToDriveCount.put(yearMonth, entity.getTotalDriveCount());
-	// 	});
-	//
-	// 	return IntStream.range(0, 12)
-	// 		.mapToObj(i -> {
-	// 			YearMonth yearMonth = twelveMonthsAgo.plusMonths(i);
-	// 			int count = monthToDriveCount.getOrDefault(yearMonth, 0);
-	// 			return new AdminBizMonthlyResponse(yearMonth.atEndOfMonth(), count);
-	// 		})
-	// 		.collect(Collectors.toList());
-	// }
 }
 
