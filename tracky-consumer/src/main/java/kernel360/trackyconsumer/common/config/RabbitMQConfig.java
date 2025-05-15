5f94992b697bbb57afff449df4e7dbbef53836d8
Tojaman <zzoon5020@naver.com>
2025-05-15 18:32:29 +0900

    feat 600개 bulk insert 및 최대 리스너 스레드 테스트

--- tracky-consumer/src/main/java/kernel360/trackyconsumer/common/config/RabbitMQConfig.java
+++ tracky-consumer/src/main/java/kernel360/trackyconsumer/common/config/RabbitMQConfig.java
@@ -3,11 +3,8 @@ package kernel360.trackyconsumer.common.config;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
@@ -39,12 +36,12 @@ public class RabbitMQConfig {
 
 		// 리스너 스레드 설정 (배치 리스너에서도 10개의 컨슈머 사용)
 		factory.setConcurrentConsumers(10);
-		factory.setMaxConcurrentConsumers(10);  // 최대 컨슈머도 동일하게 설정
+		factory.setMaxConcurrentConsumers(100);  // 최대 컨슈머도 동일하게 설정
 
 		// 메트릭 측정을 위한 설정
 		factory.setMicrometerEnabled(true);  // Micrometer 메트릭 활성화
 		factory.setObservationEnabled(true); // 관찰 활성화 (Spring Boot 3.0 이상)
-		
+
 		// 오류 핸들링 설정
 		factory.setErrorHandler(throwable -> {
 			log.error("RabbitMQ 메시지 처리 중 오류 발생: {}", throwable.getMessage(), throwable);
@@ -52,13 +49,14 @@ public class RabbitMQConfig {
 
 		factory.setDefaultRequeueRejected(properties.getBatch().isDefaultRequeueRejected());
 
-		log.info("RabbitMQ 배치 리스너 설정: concurrentConsumers={}, batchSize={}, timeout={}, enabled={}, consumerBatchEnabled={}",
+		log.info(
+			"RabbitMQ 배치 리스너 설정: concurrentConsumers={}, batchSize={}, timeout={}, enabled={}, consumerBatchEnabled={}",
 			10, properties.getBatch().getSize(), properties.getBatch().getTimeout(),
 			properties.getBatch().isEnabled(), properties.getBatch().isConsumerBatchEnabled());
-		
+
 		return factory;
 	}
-	
+
 	// 일반 리스너 컨테이너 팩토리도 정의하여 메트릭 수집 보장
 	@Bean
 	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
@@ -68,12 +66,12 @@ public class RabbitMQConfig {
 		factory.setMessageConverter(messageConverter());
 		factory.setConcurrentConsumers(5);
 		factory.setMaxConcurrentConsumers(10);
-		
+
 		factory.setMicrometerEnabled(true);
 		factory.setObservationEnabled(true);
-		
+
 		log.info("RabbitMQ 일반 리스너 설정: concurrentConsumers=5, maxConcurrentConsumers=10");
-		
+
 		return factory;
 	}
 }
